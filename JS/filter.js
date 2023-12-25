a3a23c925c73987a23200fa8eb890a0c4f2b54b3
Umang Dalvadi <umangdalvadi8@gmail.com>
2023-12-25 16:02:16 +0530

    add

---
+++ JS/filter.js
@@ -0,0 +1,7 @@
+const arr = ['umang', 'vikash', 'keval', 'dhruv', 'krishna']
+
+const a = arr.filter((x) => {
+    return x.startsWith('k')
+})
+
+console.log(a);
---
+++ JS/map.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+const a = arr.map((x) => {
+    return x * 2;
+})
+
+console.log(a)
